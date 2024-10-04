32c13e1891040e1de38233854cfa298698b0ce71
Зайчиков Демьян <demyanzaichikov@gmail.com>
2024-10-04 13:05:01 +0300

    Task n1

--- hello.pas
+++ hello.pas
@@ -1,1 +1,11 @@
-﻿writeln('hello, World!!!')
+﻿##
+var time := readinteger('Введите текущее время');
+assert((time >= 0) and (time <= 23));
+if (time >= 4) and (time <= 10) then
+  print('Доброе утро, мир!');
+if (time >= 11) and (time <= 16) then
+  print('добрый день, мир!');
+if (time >= 17) and (time <= 22) then
+  print('Добрый вечер, мир!')
+else
+  print('Доброй ночи, мир!')
