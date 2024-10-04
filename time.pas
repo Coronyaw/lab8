9f59dc305d0e4f8b7cf6f2dec39771923a649933
Зайчиков Демьян <demyanzaichikov@gmail.com>
2024-10-04 13:16:29 +0300

    Task n2

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿##
+var year := readinteger('Введите год');
+assert(year >= 0);
+if (year mod 100 <> 0) and not ((year mod 100 = 0) and (year mod 400 <> 0)) then
+  print('год високосный')
+else
+  print('Год не високосный')
