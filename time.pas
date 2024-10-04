80824485efbdf4cd36818c0b547d201e0a0c0dc6
Зайчиков Демьян <demyanzaichikov@gmail.com>
2024-10-04 13:20:20 +0300

    Изменения по условию задания 3

--- time.pas
+++ time.pas
@@ -2,6 +2,6 @@
 var year := readinteger('Введите год');
 assert(year >= 0);
 if (year mod 100 <> 0) and not ((year mod 100 = 0) and (year mod 400 <> 0)) then
-  print('год високосный')
+  print('в этом году 365 дня')
 else
-  print('Год не високосный')
+  print('в этом году 364 дней')
